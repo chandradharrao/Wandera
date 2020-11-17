6a42dde211010c955b09ded4e473057b5be17c10
Anushka Hebbar <56372418+Ash-42@users.noreply.github.com>
2020-11-17 09:47:42 +0530

    Mongoose connection handling

--- server/app.js
+++ server/app.js
@@ -10,13 +10,15 @@ const CONSTS = require("./constants");
 /* Handling connecting to Mongoose */
 // Check error in the initial connection
 mongoose.connect(CONSTS.DB_URI, CONSTS.MONGO_OPTIONS).
-    catch(error => handleError(error));
+    catch(error => {
+        console.log(`Error in connection to Mongoose. Error: ${error}`);
+    });
 // Check error after opening a connection
 mongoose.connection.on('connected', () => {
     console.log("Successfully connected to Mongoose.");
 })
 mongoose.connection.on('error', (err) => {
-    console.log(`Error in connecting to Mongoose. Error: ${err}`);
+    console.log(`Error in connection to Mongoose. Error: ${err}`);
 })
 
 app.get('/', (req, res) => {
