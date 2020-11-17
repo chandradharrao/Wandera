ecae6a9ec5f6401acd003f1b2da8c8a67787b022
Anushka Hebbar <56372418+Ash-42@users.noreply.github.com>
2020-11-17 09:22:41 +0530

    Mongoose connection handling

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 // Instantiate express module
 const express = require('express');
 const app = express();
-
 // Instantiate mongoose module
 const mongoose = require('mongoose');
-
 // Import required constant values
 const CONSTS = require("./constants");
 
+
+// Connect to Mongoose
 mongoose.connect(CONSTS.DB_URI);
+mongoose.connection.on('connected', () => {
+    console.log("Successfully connected to Mongoose.");
+})
+mongoose.connection.on('error', () => {
+    console.log("Error in connecting to Mongoose.");
+})
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World!</h1>");
