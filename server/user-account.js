b0e4684cd5e9f132ebbb5861edf08b1f30402b86
chandradharrao <chandradhar.rao@gmail.com>
2020-11-28 22:55:35 +0530

    loginAuth

--- server/user-account.js
+++ server/user-account.js
@@ -4,10 +4,16 @@ const path = require("path");
 const bcrypt = require('bcryptjs');
 const JWT = require('jsonwebtoken');
 const CONSTS = require('./constants');
+const LoginAuth = require('./MiddleWears/LoginAuth');
 
 //importing the user model as User
 const User = require('./models/wanderer');
 const mongoose = require('mongoose');
+
+//router for accessing proteted resource
+router.get('/feed',LoginAuth,(req,res)=>{//has to pass through the middle wear
+    res.json({message:"Hi user!This is your home page!"});
+})
 
 //connect to mongoose
 mongoose.connect("mongodb://localhost:27017/usersdb",{ useUnifiedTopology: true, useNewUrlParser: true });
@@ -122,8 +128,9 @@ router.post('/login-info',(req,res)=>{
                     console.log("Redirecting to homepage...!!");
                     /* JWT will be used to verify/authenticate users and identify them.JWT have the details unlike session ids that would have the reference to the detaisls tored in the server reducing the load.They are signed to prevent users from manipulating.We use jwt so that users can access only their feed or posts(protected resources) and not others. */
                     const token = JWT.sign({_id:foundData._id},CONSTS.JWT_SECRET);//replace the id of db with the signed token
-                    res.json({token : token});//this is the token gievn to the user
+                    res.json({token : token});//this is the token gievn to the user upon logging in successfully,this will be used to keep track of the usr and allow him to access protected resources
                 }
+                //https://lcalhost:3000/fed?Authorization=Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmMyODFmYzJhN2NkZDdiZThmODY0ZjQiLCJpYXQiOjE2MDY1ODMwMjR9.hTT5XrHIsa9j21voechWSTzOBxGGWw5CNJSpt7S6SYM
                 else{
                     console.log("Your username and password didnt match!");
                     return res.status(422).json({message:'Your username and password didnt match'})
